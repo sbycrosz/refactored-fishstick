ae1881dda80034df56db5910a52e6136274fe92f
Sam Aryasa <sambya@aryasa.net>
2023-10-27 13:47:09 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState, useCallback, useEffect} from 'react';
+
+type RemoteThemeConfig = {
+    name: string;
+    parentTheme: string;
+    assets: Record<string, string>;
+    colors: Record<string, string>;
+    config: Record<string, string>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const getRemoteThemes = useCallback(async () => {
+        const result = await fetch(
+            'https://crossorigin.me/https://api.github.com/gists/4babfc04af795c2c195a004e72c66233',
+            {
+                headers: {
+                    Authorization: 'Bearer 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',
+                },
+            },
+        );
+        const gist = await result.json();
+        return JSON.parse(gist.files['themes.json'].content).themes as RemoteThemeConfig[];
+    }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [remoteThemes, setRemoteThemes] = useState<RemoteThemeConfig[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const _remoteThemes = await getRemoteThemes();
+            setRemoteThemes(_remoteThemes);
+        })();
+    }, [setRemoteThemes, getRemoteThemes]);
+
+    return (
+        <>
+            <div>
+                {JSON.stringify(remoteThemes, undefined, 2)}
+                <input></input>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
